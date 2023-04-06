6919bb30b0b3864d98ea6e17b8b258f418bf97fa
Gsarault44 <gregsarault@gmail.com>
2023-04-06 12:50:10 -0400

    google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -10,6 +11,21 @@ export default function Document() {
           content="Just a portfolio site about me."
           key="desc"
         />
+        <Script strategy="afterInteractive" src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXX"/>
+        <Script
+          id='google-analytics'
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-G4LKQ715LP', {
+          page_path: window.location.pathname,
+          });
+          `,
+          }}
+          />
       </Head>
       <body>
         <Main />
